e8a33ee7221da54e226c547358b385b3d0073c7f
e <2652008969@qq.com>
2018-12-18 22:14:25 +0800

    改了方法可以了

--- python/读入一个正整数 n，计算其各位数字之和，用汉语拼音写出和的每一位数字.py
+++ python/读入一个正整数 n，计算其各位数字之和，用汉语拼音写出和的每一位数字.py
@@ -1,38 +1,40 @@
 def sum (a) :
     if a==0 :
-       print(" ling")
+       print("ling",end=" ")
     elif a==1 :
-        print(" yi")
+        print("yi",end=" ")
     elif a==2 :
-        print(" er")
+        print("er",end=" ")
     elif a==3 :
-         print(" san")
+         print("san",end=" ")
     elif a==4 :
-        print(" si")        
+        print("si",end=" ")        
     elif a==5 :
-         print(" wu")
+         print("wu",end=" ")
     elif a==6 :
-         print(" liu")
+         print("liu",end=" ")
     elif a==7 :
-         print(" qi")        
+         print("qi",end=" ")        
     elif a==8 :
-        print(" ba")
+        print("ba",end=" ")
     elif a==9 :
-         print(" jiu")
+         print("jiu",end=" ")
+
 b=int(input())
 c=0
 while b>0 :
     c=c+b%10
-    b=int(int(b)/10)
-    print(c)
+    b=b//10
 d=[]
 i=0
 while c>=1 :
-    d.append(int(c%10))
-    c=int(c/10)
+    d.append(c%10)
+    c=c//10
     i=i+1
-print(d)
 while i>=1 :
     a=d[i-1]
     sum(a)
+    if i!=1 :
+        print(" ",end="")
     i=i-1
+print()

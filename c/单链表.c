6360a2704b4c2b8f6e3daecfd9dffbbaec5b1979
scar <2652008969@qqcom>
2019-02-24 21:31:32 +0800

    上次代码好像有问题 修复了一下

--- c/单链表.c
+++ c/单链表.c
@@ -1,24 +1,34 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<stdlib.h>
+#define N 10
  int i; 
+ int j;
+ int k;
 int main()
 {
     struct test
      {
          int mark;
-         char name[10];
+         int ID;
+         char name[N];
          struct test *p;
-     }*b，*c;
+     }*b,*c;
      struct test *frist=NULL;
      struct test *pp;
-     for(i=0;i<3;i++)
+     printf("请输入录入学生数据的个数");
+     scanf("%d",&j);
+     for(i=0;i<j;i++)
       {  getchar();
          pp=malloc(sizeof(struct test));
-         scanf("%d %s",&(pp->mark),&(pp->name));
+         scanf("%d%d%s",&(pp->ID),&(pp->mark),&(pp->name));
          pp->p=frist;
          frist=pp;
       }          //for循环是为了创建结点并且插入链表 frist 
-      for(b=NULL,c=frist;c!=NULL&&c->name!='z';b=c,c=c->p);  //定位要删除的姓名为z的数据
-      b->p=c->c;
-      free(c); //删除结点
+      printf("请输入查找学生的Id");
+      scanf("%d",&k);
+      for(b=NULL,c=frist;c!=NULL&&c->ID!=k;b=c,c=c->p);  
+      {
+
+      }
+      printf("%s\n%d",(c->name),(c->mark));
 }
